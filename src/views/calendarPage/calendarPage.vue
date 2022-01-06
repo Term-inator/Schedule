933789a81c900e6a62e7a73cc3106295b91e30cc
Term-inator <1121745627@qq.com>
2022-01-06 17:00:38 +0800

    d3

--- src/views/calendarPage/calendarPage.vue
+++ src/views/calendarPage/calendarPage.vue
@@ -1,16 +1,56 @@
 <template>
-  
+	<div class="main">
+		<Card style="width: 100%; height: 100%">
+			<div slot="title">
+				<Row type="flex" justify="space-between" align="middle">
+					<Button shape="circle" icon="ios-arrow-back"></Button>
+					{{ month }}
+					<Button shape="circle" icon="ios-arrow-forward"></Button>
+				</Row>
+			</div>
+			<div id="days">
+
+			</div>
+		</Card>
+	</div>
 </template>
 
 <script>
+import * as d3 from 'd3'
+
 export default {
   name: "calendarPage",
+	data() {
+		return {
+			month: 1,
+			now: new Date()
+		}
+	},
 	mounted() {
-		console.log("calendar")
+		this.createCalendar()
+		setInterval(() => {
+			this.updateTime()
+			this.createCalendar()
+		}, 20001)
+	},
+	methods: {
+		updateTime() {
+			let now = new Date()
+			if(this.now.getHours() !== now.getHours() || this.now.getMinutes() !== now.getMinutes) {
+				this.now = now
+			}
+			console.log(this.now)
+		},
+		createCalendar() {
+			let days = d3.select("#days")
+			console.log(days)
+		}
 	}
 }
 </script>
 
-<style>
-
+<style scoped>
+.main {
+	padding: 5vh 5vw 0 5vw;
+}
 </style>
