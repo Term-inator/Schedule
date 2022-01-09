76d3e9564531cd72863072444fc3a1ddd4e06680
Term-inator <1121745627@qq.com>
2022-01-10 00:27:21 +0800

    add tasks

--- src/views/addSchedulePage/addSchedulePage.vue
+++ src/views/addSchedulePage/addSchedulePage.vue
@@ -20,6 +20,7 @@ import scheduleLexer from "../../parser/scheduleLexer.js"
 import scheduleParser from "../../parser/scheduleParser.js"
 import scheduleListener from "../../parser/scheduleListener.js"
 import scheduleErrListener from "../../parser/scheduleErrListener.js"
+import { customAlphabet } from "nanoid"
 
 export default {
   name: "addSchedulePage",
@@ -51,7 +52,7 @@ export default {
 			const rename_test = "rename 测试 测试1; rename id 123Tt4444 测试2;"
 			const ajust_test = "ajust 2022 01/09 12:00-13:00 测试 to 13:00-14:00; ajust id 1245553 to 13:20-14:00; ajust id 1214452 to 2022 01/09 13:20-14:00; ajust id 122722 to 01/09 13:20-14:00;"
 			const test = add_test + del_test + delall_test + rename_test + ajust_test
-			const input = "add 2022 01/09,01/10 12:00-13:30, 14:00-14:30 测试test;"
+			const input = add_test
 			const chars = new antlr4.InputStream(input)
 			const lexer = new scheduleLexer(chars)
 			const tokens  = new antlr4.CommonTokenStream(lexer)
@@ -78,18 +79,60 @@ export default {
 			this.operate()
 		},
 		operate() {
+			let week = [null, "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+
+			class Task {
+				id = null
+				name = null
+				time_ranges = null
+			}
+
+			let tasks_json = {}
+
 			for(let op in this.tasks) {
 				this.tasks[op].forEach((obj) => {
 					switch(op) {
 						case "add": {
-							let task = obj
+							const nanoid = customAlphabet('0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz', 8)
 							// add YEAR dates timeranges NAME;
-							if(task.year !== null) {
-								
+							if(obj.year !== null) {
+								obj.dates.forEach((date) => {
+									obj.dates.forEach((date) => {
+										let task = new Task()
+										task.id = nanoid()
+										let time = obj.year + "/" + date
+										task.name = obj.names[0]
+										task.time_ranges = obj.time_ranges
+										if(tasks_json[time] === undefined) {
+											tasks_json[time] = []
+										}
+										tasks_json[time].push(task)
+									})
+								})
 							}
 							// add daterange weekdays? timeranges NAME;
 							else {
-
+								let task = new Task()
+								task.id = nanoid()
+								task.name = obj.names[0]
+								task.time_ranges = obj.time_ranges
+								let date_range = obj.date_range
+								let start = new Date(date_range.substring(1, 5) + "/" + date_range.substring(5, 10))
+								let end = new Date(date_range.substring(11, 15) + "/" + date_range.substring(15, 20))
+								
+								while((end.getTime() - start.getTime())>=0){
+									let year = start.getFullYear()
+									let month = (start.getMonth() + 1).toString().length === 1 ? "0" + (start.getMonth() + 1).toString() : (start.getMonth() + 1).toString()
+									let date = start.getDate().toString().length === 1 ? "0" + start.getDate() : start.getDate()
+									if(obj.week_days.length === 0 || obj.week_days.indexOf(week[start.getDay()]) !== -1) {
+										let time = year + '/' + month + '/' + date
+										if(tasks_json[time] === undefined) {
+											tasks_json[time] = []
+										}
+										tasks_json[time].push(task)
+									}
+									start.setDate(start.getDate() + 1)
+								}
 							}
 							break
 						}
@@ -109,6 +152,7 @@ export default {
 					}
 				})
 			}
+			console.log(tasks_json)
 		}
 	}
 }
