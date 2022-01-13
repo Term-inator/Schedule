86cc3e055f088780b1dc717d7b84e667cf0b478c
Term-inator <1121745627@qq.com>
2022-01-13 22:38:00 +0800

    tests

--- src/views/addSchedulePage/addSchedulePage.vue
+++ src/views/addSchedulePage/addSchedulePage.vue
@@ -40,7 +40,34 @@ export default {
 			const delall_test = "delall 2022 01/09, 01/17; delall (2022 01/09, 2023 01/09); delall 测试; delall (2022 01/09, 2023 01/09) 测试; delall 12:00-13:00, 14:00-15:00; delall id #12345666, #23565656;"
 			const rename_test = "rename 测试 测试1; rename id #123Tt4444 测试2;"
 			const ajust_test = "ajust 2022 01/09 12:00-13:00 测试 to 13:00-14:00; ajust id #1245553 to 13:20-14:00; ajust id #1214452 to 2022 01/09 13:20-14:00; ajust id #122722 to 01/09 13:20-14:00;"
-			const test = add_test + del_test + delall_test + rename_test + ajust_test
+			
+			const task1 = "2022 01/09,01/10 12:00-13:30, 14:00-14:30 测试test"
+			const task2 = "(2022 01/09, 2022 01/18) Mon, Tue 12:00-13:00 test_测试"
+
+			const add_task1 = "add "+ task1 + ";"
+			const add_task2 = "add" + task2 + ";"
+			const add_task12 = "add" + task1 + "&" + task2 + ";"
+
+			const del_task1 = add_task1 + "del" + task1 + ";"
+			const del_task2 = add_task2 + "del" + task2 + ";"
+			const del_id_test = ""
+
+			const delall_year_date = add_task12 + "delall 2022 01/09, 01/10, 01/11, 01/17;"
+			const delall_daterange = add_task12 + "delall (2022 01/09, 2022 01/18);"
+			const delall_daterange_names = add_task12 + "delall (2022 01/09, 2022 01/18) 测试test, test_测试;"
+			const delall_daterange_timeranges = add_task12 + "delall (2022 01/09, 2022 01/18) 12:00-13:00, 14:00-14:30;"
+			const delall_names = add_task12 + "delall 测试test, test_测试;"
+			const delall_timeranges = add_task12 + "delall 12:00-13:00, 14:00-14:30;"
+			const delall_ids = ""
+
+			const rename_name_name = add_task12 + "rename 测试test test_测试;" + "delall test_测试;"
+			const rename_id_name = ""
+
+			const ajust_task1_year_date_timerange = ""
+			const ajust_task1_date_timerange = ""
+			const ajust_task1_timerange = ""
+			const ajust_task2_year_date_timerange = ""
+
 			const input = "add 2022 01/09,01/10 12:00-13:30, 14:00-14:30 测试test;"
 			const chars = new antlr4.InputStream(input)
 			const lexer = new scheduleLexer(chars)
